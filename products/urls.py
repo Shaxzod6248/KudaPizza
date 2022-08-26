e43abe720282434ef590f8f562f6a2bd9530eb02
Shaxzod <painloversgame@gmail.com>
2022-08-26 14:46:06 +0500

    fix

--- products/urls.py
+++ products/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,)
-
 
 
 router = DefaultRouter()
